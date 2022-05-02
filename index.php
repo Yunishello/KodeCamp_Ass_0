1d3d2179d95329befcd1daf5a99c993ec54f0007
yunishello <yunishello@gmail.com>
2022-05-02 10:32:51 -0700

    KodeCamp first assignment

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     //geting operand and oprator from value from console
     $operand_a = (int)readline("Enter Your First Value: ");
     $operand_b = (int)readline("Enter Your Second Value: ");
-    echo("Enter Value Of Operator The Follow Options: \n 1 for + \n 2 for - \n 3 for * \n 4 for / \n");
+    echo("Enter Value Of Operator The Follow Options: \n 1 for + \n 2 for - \n 3 for * \n 4 for / \n 5 for % \n");
     $operator = readline("Enter The Operator To Calculate With: ");
     if ($operator == "1") {
         $result = $operand_a + $operand_b;
@@ -16,6 +16,9 @@
     }elseif ($operator == "4") {
         $result = $operand_a / $operand_b;
         echo("The division of ".$operand_a. " and ".$operand_b. " = ". $result);
+    }elseif ($operator == "5") {
+        $result = $operand_a % $operand_b;
+        echo("The modulus of ".$operand_a. " and ".$operand_b. " = ". $result);
     }else {
         echo("Please Enter An Operator");
     }
